f2482983fc39940f75d0c4225760329de69e42d0
sophiayangx <sopyang@nuevaschool.org>
2016-10-17 08:36:43 -0700

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,9 @@
+var person = {
+  name: "David",
+  representation: "http://i.dailymail.co.uk/i/pix/2016/03/14/19/322C3FA100000578-0-image-a-13_1457982438669.jpg",
+  height: "500px",	
+  favoriteColor: "blue",
+  catchphrase: "Get pumped!"
+}
+
+//drawPerson(person)
