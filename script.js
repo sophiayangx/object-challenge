6762b7132fac930eaf6a8cd09efb6c3994b43a7c
sophiayangx <sopyang@nuevaschool.org>
2016-10-17 08:53:40 -0700

    Classadoo autocommit

---
+++ script.js
@@ -0,0 +1,17 @@
+console.log()
+
+var person = {
+  name: "Sophia",
+  representation: "https://s-media-cache-ak0.pinimg.com/236x/eb/5c/78/eb5c78657282a7c7715939aac4553dcb.jpg",
+  height: "500px",	
+  favoriteColor: "blue",
+  catchphrase: "Woohoo!"
+}
+
+drawPerson(person)
+
+function drawPerson(person) {
+  person.catchphrase
+}
+  
+drawPerson("Sophia")
